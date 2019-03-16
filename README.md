f5a74f816895d30c8087fad1302312acdf56a105
rkbk60 <b8csmlplg@gmail.com>
2019-03-16 17:48:06 +0900

    update emacs-nt/cica/nyagos

---
+++ README.md
@@ -0,0 +1,59 @@
+# Scoop for jp [![test on AppVeyor](https://ci.appveyor.com/api/projects/status/d8wbibud44gg8toi?svg=true&failingText=Test%3Afailing&passingText=Test%3Apassing)](https://ci.appveyor.com/project/rkbk60/scoop-for-jp)
+
+日本語環境で使用するポータブルアプリやフォントなどを寄せ集めた[Scoop](https://github.com/lukesampson/scoop)の非公式Bucketです。  
+非技術者含む幅広い日本人を対象した、`main`や`extras`など基本的なBucketの拡張にすることを目的としています。
+
+※ このリポジトリはGitHubが利用できないために作られた一時的なものです。問題が解決し次第、[GitHub版](https://github.com/rkbk60/scoop-for-jp.git)へ統合されます。
+
+## 収録Manifest
+
+### アプリ
+- NTEmacs: `emacs-nt`
+- Nyagos: `nyagos`
+- KaoriYa版Vim: `vim-kaoriya`
+
+### フォント
+- Cica: `cica`
+- Myrica: `myrica`
+- MyricaM: `myrica-m`
+- Ricty Diminished: `ricty-diminished`
+- Source Han Code JP: `source-han-code-jp`
+
+### 予定
+
+- 秀丸エディタ
+- TeraX(TeraTerm, TeraPad...)
+- SKKツール各種
+- SKK辞書
+- WinMerge日本語版
+- フォント各種(OpenTypeやプロポーショナル版含む)
+
+## 使い方
+
+Bucket有効化
+```
+scoop bucket add jp https://github.com/rkbk60/scoop-for-jp
+```
+
+アプリの追加
+```
+scoop install emacs-nt vim-kaoriya
+```
+
+フォントの追加(全自動)
+```
+scoop install main/sudo
+sudo scoop install cica -g
+```
+
+フォントの追加(半自動、展開先は環境変数`JP_FONT_DIR`で指定可能)
+```
+scoop install cica
+explorer %USERPROFILE%\JpFonts
+```
+
+## 追加・修正・削除など
+
+すべて当リポジトリのIssuesにて受け付けています。  
+[CONTRIBUTING](https://github.com/rkbk60/scoop-for-jp/blob/master/CONTRIBUTING.md)も作りましたので、ぜひご覧ください。
+
---
+++ psutil/font.ps1
@@ -0,0 +1,113 @@
+# フォントのインストール/アンインストール用スクリプト
+# 管理者権限の有無で以下のように処理内容を変化
+# - 管理者: レジストリ登録
+# - 非管理者: 専用ディレクトリへコピー
+#
+# 引数
+#   1: インストールか否か($true or $false)
+#   2: $dir(解凍したディレクトリへのパス)
+#   3: フォント名を示すワイルドカード
+#
+# 環境変数
+#   JP_FONT_DIR: ローカルインストール時に利用するディレクトリ
+#                デフォルト値: "$HOME\JpFonts"
+
+Param($is_install, $dir, $wildcard)
+
+function is_admin {
+    $admin = [security.principal.windowsbuiltinrole]::administrator
+    $id = [security.principal.windowsidentity]::getcurrent()
+    ([security.principal.windowsprincipal]($id)).isinrole($admin)
+}
+function warn {
+    Param($msg)
+    Write-Host "Warning: $msg" -ForegroundColor Red
+}
+function notice {
+    Param($msg)
+    Write-Host $msg -ForegroundColor Magenta
+}
+function log {
+    Param($msg)
+    Write-Host $msg -ForegroundColor White
+}
+
+function install_global {
+    Param($dir, $wildcard)
+    $regist = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts'
+    $postfix = ' (TrueType)'
+    Get-ChildItem $dir -Filter $wildcard | ForEach-Object {
+        log "registering $_ ..."
+        New-ItemProperty -Path $regist `
+                         -Name $_.Name.Replace($_.Extension, $postfix)`
+                         -Value $_.Name `
+                         -Force `
+            | Out-Null
+        Copy-Item "$dir\$_" -Destination "$env:windir\Fonts"
+    }
+}
+function uninstall_global {
+    Param($dir, $wildcard)
+    $regist = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts'
+    $postfix = ' (TrueType)'
+    Get-ChildItem $dir -Filter $wildcard | ForEach-Object {
+        log "unregistering $_ ..."
+        Remove-ItemProperty -Path $regist `
+                            -Name $_.Name.Replace($_.Extension, $postfix) `
+                            -ErrorAction SilentlyContinue `
+                            -Force
+        Remove-Item "$env:windir\Fonts\$($_.Name)" -ErrorAction SilentlyContinue -Force
+    }
+}
+
+function install_local {
+    Param($dir, $wildcard)
+    $pool = [environment]::GetEnvironmentVariable('JP_FONT_DIR', 'User')
+    if ($pool -eq $null -or $pool -eq "") {
+        $pool = "$HOME\JpFonts"
+        [environment]::SetEnvironmentVariable('JP_FONT_DIR', "$HOME\JpFonts", 'User')
+    }
+    if (!(Test-Path $pool)) {
+        notice "Make new directory '$pool' to access font files easily."
+        New-Item -Path $pool -ItemType Directory -Force | Out-Null
+    }
+    Get-ChildItem $dir -Filter $wildcard | ForEach-Object {
+        log "making copy of $_ ..."
+        Copy-Item "$dir\$_" -Destination $pool
+    }
+    notice "Font files are copied to '$pool'."
+    notice "You can access this directory from THE NEXT SESSION with these commands:"
+    notice '- cmd.exe     explorer %JP_FONT_DIR%'
+    notice '- PowerShell  explorer $env:JP_FONT_DIR'
+}
+function uninstall_local {
+    Param($dir, $wildcard)
+    $pool = [environment]::GetEnvironmentVariable('JP_FONT_DIR', 'User')
+    if ($pool -eq $null) {
+        warn "%JP_FONT_DIR% is not defined. End uninstallation."
+        return 1
+    }
+    if (!(Test-Path $pool)) {
+        warn "%JP_FONT_DIR% is not existing directory. End uninstallation."
+        return 2
+    }
+    Get-ChildItem $dir -Filter $wildcard | ForEach-Object {
+        log "deleting copy of $_ ..."
+        Remove-Item "$pool\$($_.Name)" -ErrorAction SilentlyContinue -Force
+    }
+}
+
+# main
+if (is_admin) {
+    if ($is_install) {
+        install_global   $dir $wildcard
+    } else {
+        uninstall_global $dir $wildcard
+    }
+} else {
+    if ($is_install) {
+        install_local    $dir $wildcard
+    } else {
+        uninstall_local  $dir $wildcard
+    }
+}
