bd333387eed00f5003782fb0e8f801eec66dc608
rkbk60 <b8csmlplg@gmail.com>
2020-05-21 08:36:37 +0900

    update: support user-local font install

--- psutil/font.ps1
+++ psutil/font.ps1
@@ -19,6 +19,10 @@ function is_admin {
     $id = [security.principal.windowsidentity]::getcurrent()
     ([security.principal.windowsprincipal]($id)).isinrole($admin)
 }
+function can_use_local_font {
+    $buildnum = Get-WmiObject Win32_OperatingSystem | Select-Object BuildNumber
+    return $buildnum.BuildNumber -ge 17134
+}
 function warn {
     Param($msg)
     Write-Host "Warning: $msg" -ForegroundColor Red
@@ -32,68 +36,91 @@ function log {
     Write-Host $msg -ForegroundColor White
 }
 
-function install_global {
-    Param($dir, $wildcard)
-    $regist = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts'
+function install_ttf_fonts {
+    Param($fontdir, $target, $wildcard)
     $postfix = ' (TrueType)'
-    Get-ChildItem $dir -Filter $wildcard | ForEach-Object {
+    Get-ChildItem $fontdir -Filter $wildcard | ForEach-Object {
         log "registering $_ ..."
         New-ItemProperty -Path $regist `
                          -Name $_.Name.Replace($_.Extension, $postfix)`
                          -Value $_.Name `
                          -Force `
             | Out-Null
-        Copy-Item "$dir\$_" -Destination "$env:windir\Fonts"
+        Copy-Item "$fontdir\$_" -Destination "$target\"
     }
 }
-function uninstall_global {
-    Param($dir, $wildcard)
-    $regist = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts'
+function uninstall_ttf_fonts {
+    Param($fontdir, $target, $wildcard)
     $postfix = ' (TrueType)'
-    Get-ChildItem $dir -Filter $wildcard | ForEach-Object {
+    Get-ChildItem $fontdir -Filter $wildcard | ForEach-Object {
         log "unregistering $_ ..."
         Remove-ItemProperty -Path $regist `
                             -Name $_.Name.Replace($_.Extension, $postfix) `
                             -ErrorAction SilentlyContinue `
                             -Force
-        Remove-Item "$env:windir\Fonts\$($_.Name)" -ErrorAction SilentlyContinue -Force
+        Remove-Item "$target\$($_.Name)" -ErrorAction SilentlyContinue -Force
     }
+}
+
+function install_global {
+    Param($dir, $wildcard)
+    $regist = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts'
+    install_ttf_fonts $dir $regist $wildcard
+}
+function uninstall_global {
+    Param($dir, $wildcard)
+    $regist = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts'
+    uninstall_ttf_fonts $dir $regist $wildcard
 }
 
 function install_local {
     Param($dir, $wildcard)
-    $pool = [environment]::GetEnvironmentVariable('JP_FONT_DIR', 'User')
-    if ($pool -eq $null -or $pool -eq "") {
-        $pool = "$HOME\JpFonts"
-        [environment]::SetEnvironmentVariable('JP_FONT_DIR', "$HOME\JpFonts", 'User')
+    if (can_use_local_font) {
+        # newer than 1803
+        $local = [environment]::GetEnvironmentVariables('LocalAppData', 'User')
+        install_ttf_fonts $dir "$local\Microsoft\Windows\Fonts" $wildcard
+        notice "Font files has installed to local."
+        notice "These fonts are only for you."
+    } else {
+        $pool = [environment]::GetEnvironmentVariable('JP_FONT_DIR', 'User')
+        if ($pool -eq $null -or $pool -eq "") {
+            $pool = "$HOME\JpFonts"
+            [environment]::SetEnvironmentVariable('JP_FONT_DIR', "$HOME\JpFonts", 'User')
+        }
+        if (!(Test-Path $pool)) {
+            notice "Make new directory '$pool' to access font files easily."
+            New-Item -Path $pool -ItemType Directory -Force | Out-Null
+        }
+        Get-ChildItem $dir -Filter $wildcard | ForEach-Object {
+            log "making copy of $_ ..."
+            Copy-Item "$dir\$_" -Destination $pool
+        }
+        notice "Font files are copied to '$pool'."
+        notice "You can access this directory from THE NEXT SESSION with these commands:"
+        notice '- cmd.exe     explorer %JP_FONT_DIR%'
+        notice '- PowerShell  explorer $env:JP_FONT_DIR'
     }
-    if (!(Test-Path $pool)) {
-        notice "Make new directory '$pool' to access font files easily."
-        New-Item -Path $pool -ItemType Directory -Force | Out-Null
-    }
-    Get-ChildItem $dir -Filter $wildcard | ForEach-Object {
-        log "making copy of $_ ..."
-        Copy-Item "$dir\$_" -Destination $pool
-    }
-    notice "Font files are copied to '$pool'."
-    notice "You can access this directory from THE NEXT SESSION with these commands:"
-    notice '- cmd.exe     explorer %JP_FONT_DIR%'
-    notice '- PowerShell  explorer $env:JP_FONT_DIR'
 }
 function uninstall_local {
     Param($dir, $wildcard)
-    $pool = [environment]::GetEnvironmentVariable('JP_FONT_DIR', 'User')
-    if ($pool -eq $null) {
-        warn "%JP_FONT_DIR% is not defined. End uninstallation."
-        return 1
-    }
-    if (!(Test-Path $pool)) {
-        warn "%JP_FONT_DIR% is not existing directory. End uninstallation."
-        return 2
-    }
-    Get-ChildItem $dir -Filter $wildcard | ForEach-Object {
-        log "deleting copy of $_ ..."
-        Remove-Item "$pool\$($_.Name)" -ErrorAction SilentlyContinue -Force
+    if (can_use_local_font) {
+        # newer than 1803
+        $local = [environment]::GetEnvironmentVariables('LocalAppData', 'User')
+        uninstall_ttf_fonts $dir "$local\Microsoft\Windows\Fonts" $wildcard
+    } else {
+        $pool = [environment]::GetEnvironmentVariable('JP_FONT_DIR', 'User')
+        if ($pool -eq $null) {
+            warn "%JP_FONT_DIR% is not defined. End uninstallation."
+            return 1
+        }
+        if (!(Test-Path $pool)) {
+            warn "%JP_FONT_DIR% is not existing directory. End uninstallation."
+            return 2
+        }
+        Get-ChildItem $dir -Filter $wildcard | ForEach-Object {
+            log "deleting copy of $_ ..."
+            Remove-Item "$pool\$($_.Name)" -ErrorAction SilentlyContinue -Force
+        }
     }
 }
 
